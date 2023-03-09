d5e96214dc09a33b1a37d4bd7bc1b0225338fbad
kkwh <121307297+kkwh@users.noreply.github.com>
2023-03-09 19:35:51 +0900

    Add files via upload

--- Java04_Array/src/edu/java/array09/ArrayMain09.java
+++ Java04_Array/src/edu/java/array09/ArrayMain09.java
@@ -1,6 +1,5 @@
 package edu.java.array09;
 
-import java.util.Iterator;
 import java.util.Random;
 
 public class ArrayMain09 {
@@ -10,19 +9,25 @@ public class ArrayMain09 {
 		int[][] arr = new int[3][];
 
 		// 첫번째 배열에는 난수 2개, 두번째 배열에는 난수 3개, 세번째 배열에는 난수 4개 저장.
-		arr[0] = new int[2];
-		arr[1] = new int[3];
-		arr[2] = new int[4];
-		int count = 0;
-
 		// 저장하는 난수의 범위는 0 이상 100 이하.
+//		arr[0] = new int[2];
+//		arr[1] = new int[3];
+//		arr[2] = new int[4];
 		Random rand = new Random();
 		for(int i=0; i<arr.length; i++) {
+			arr[i] = new int[i+2];
 			for(int j=0; j<arr[i].length; j++) {
 				arr[i][j] = rand.nextInt(101);
-				count++;
 			}
 		}
+
+//		Random rand = new Random();
+//		for(int i=0; i<arr.length; i++) {
+//			for(int j=0; j<arr[i].length; j++) {
+//				arr[i][j] = rand.nextInt(101);
+//				
+//			}
+//		}
 
 		// 2차원 배열의 원소들을 출력.
 		for(int x[] : arr) {
@@ -35,14 +40,17 @@ public class ArrayMain09 {
 
 		// 2차원 배열의 모든 원소들의 합을 계산하고 출력.
 		int sum = 0;
+		int count = 0;
 		for(int i=0; i<arr.length; i++) {
 			for(int j=0; j<arr[i].length; j++) {
 				sum += arr[i][j];
+				count++;
 			}
 		}
 		//    	for(int x[] : arr) {
 		//    		for(int y : x) {
 		//    			sum += y;
+		//				count++;
 		//    		}
 		//    	}
 		System.out.println("모든 원소들의 합 : " + sum);
@@ -53,38 +61,40 @@ public class ArrayMain09 {
 
 		// 최댓값을 찾고 출력.
 		int max = 0;
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr[i].length; j++) {
-				if(arr[i][j] > max) {
-					max = arr[i][j];
-				}
-			}
-		}
-		//    	for(int[] x : arr) {
-		//    		for(int y : x) {
-		//    			if(y > max) {
-		//    				max = y;
-		//     			}
-		//    		}
-		//    	}
+//		for(int i=0; i<arr.length; i++) {
+//			for(int j=0; j<arr[i].length; j++) {
+//				if(arr[i][j] > max) {
+//					max = arr[i][j];
+//				}
+//			}
+//		}
+		    	for(int[] x : arr) { // 2차원 배열의 원소인 1차원 배열들에 대해서
+		    		for(int y : x) {  // 1차원 배열의 각 원소들에 대해서
+//		    			if(y > max) {
+//		    				max = y;
+//		     			}
+		    			max = (y > max) ? y : max;
+		    		}
+		    	}
 		System.out.println("최댓값 : " + max);
 
 		// 최솟값을 찾고 출력.
 		int min = arr[0][0];
-		for(int i=0; i<arr.length; i++) {
-			for(int j=0; j<arr[i].length; j++) {
-				if(arr[i][j] < min) {
-					min = arr[i][j];
-				}
-			}
-		}
-		//    	for(int[] x : arr) {
-		//    		for(int y : x) {
-		//    			if(y < min) {
-		//    				min = y;
-		//     			}
-		//    		}
-		//    	}
+//		for(int i=0; i<arr.length; i++) {
+//			for(int j=0; j<arr[i].length; j++) {
+//				if(arr[i][j] < min) {
+//					min = arr[i][j];
+//				}
+//			}
+//		}
+		    	for(int[] x : arr) {
+		    		for(int y : x) {
+//		    			if(y < min) {
+//		    				min = y;
+//		     			}
+		    			min = (y < min) ? y : min;
+		    		}
+		    	}
 		System.out.println("최솟값 : " + min);
 		
 		int columnSum1 = 0;
